d2ee32befa3b8f867674efdcaa42a89629416368
d <dummyunt123@gmail.com>
2024-08-27 03:02:45 +0530

    commit

---
+++ routes/cityRoutes.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+// src/routes/cityRoutes.js
+const express = require('express');
+const {
+    addCity,
+    updateCity,
+    deleteCity,
+    getCities
+} = require('../controllers/cityController');
+
+const router = express.Router();
+
+router.post('/cities', addCity);
+router.put('/cities/:id', updateCity);
+router.get('/cities', getCities);
+router.delete('/cities/:id', deleteCity);
+
+module.exports = router;
